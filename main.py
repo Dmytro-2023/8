51a7c1068b65625b36f667063484ad611159658c
Dmytro-2023 <129963556+Dmytro-2023@users.noreply.github.com>
2023-04-22 21:07:44 +0300

    Create main.py
    1 function

---
+++ main.py
@@ -0,0 +1,7 @@
+def main_function():
+    # Step 1: Define variables
+    name = "Dmytro"
+    age = 44
+    
+    # Step 2: Print a message
+    print(f"Hello, my name is {name} and I am {age} years old.")
